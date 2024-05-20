1f671e915f46dc80c89317c6720ad3ab4946dd79
acascell <infocascella@gmail.com>
2024-05-20 13:55:25 +0200

    feat: experimenting components defining multiple items like counter, ul list etc.. using react parent child architecture

--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,8 +1,21 @@
 import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+import Counter from "./components/Counter"
+import List from "./components/List"
+
+import { useState } from "react"
+
 function App() {
+  const [count, setCount] = useState<number>(1)
 
   return (
-    <Heading title="Hello!"/>
+    <>
+      <Heading title="Hello!"/>
+      <Section title={"Different title"}>This is my section.</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List items={["Coffee", "Takos", "Code"]} render={(item:
+      string) => <span className="gold">{item}</span>} />
+    </>
   )
 }
 
